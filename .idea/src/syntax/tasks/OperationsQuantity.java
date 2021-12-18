92e018729d3ad15cc301f5b925f48d8dc56e734d
Valentin Rachkov <noslav7@gmail.com>
2021-12-18 22:55:52 +0500

    commit

---
+++ .idea/src/syntax/tasks/OperationsQuantity.java
@@ -0,0 +1,25 @@
+package syntax.ifelse;
+
+import java.util.Scanner;
+
+public class OperationsQuantity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        int multiply = 0;
+        int plus = 0;
+        int minus = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '*') {
+                multiply++;
+            } else if (expression.charAt(i) == '+') {
+                plus++;
+            } else if (expression.charAt(i) == '-') {
+                minus++;
+            }
+        }
+        System.out.println("* " + multiply);
+        System.out.println("+ " + plus);
+        System.out.println("- " + minus);
+    }
+}
